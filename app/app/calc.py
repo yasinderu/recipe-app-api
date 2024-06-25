6c59f2f517291eb16b78cc2120810c10112d7fe2
yasinderu <m.yasin449@gmail.com>
2024-06-25 14:09:34 +0700

    Add postgres database

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y) -> int:
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x, y) -> int:
+    """Subtract x from y and return result."""
+    return y - x
